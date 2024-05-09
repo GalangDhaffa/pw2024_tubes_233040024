d9922c7caa31cd4afe02493181bf57d41d905531
GalangDhaffa <davagalang3456@gmail.com>
2024-05-09 23:12:34 +0700

    menambahkan  front end 1

---
+++ index.php
@@ -0,0 +1,555 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Secreto Custom Prestige</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/screen.css" />
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Josefin+Sans&family=Lilita+One&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+      integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+    />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+  </head>
+  <body>
+    <!-- NAVBAR -->
+    <nav class="bg-navbar navbar navbar-expand-lg position-sticky top-0 z-1">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img
+            src="img/logo/logo6.gif"
+            alt="Logo"
+            width="60"
+            height="60"
+            class="d-inline-block align-text-top"
+          />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ms-auto">
+            <a class="nav-link active link" aria-current="page" href="#home"
+              >Home</a
+            >
+            <a class="nav-link link" href="#about">About</a>
+            <a class="nav-link link" href="#gallery">Gallery</a>
+            <a class="nav-link link" href="#produk">Produk</a>
+            <a class="nav-link link" href="#contact">Contact</a>
+            <a href="#" class="btn"
+              ><span class="material-symbols-outlined"> shopping_cart </span></a
+            >
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <!-- HOME -->
+    <section id="home">
+      <div
+        id="carouselExampleSlidesOnly"
+        class="carousel slide home"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="img/G1.png"
+              class="gambar-home d-flex w-200 opacity-75 "
+              height="855"
+              alt="pic1"
+            />
+            <div
+              class="carousel-caption position-absolute text-center home-text d-flex flex-wrap"
+            >
+              <img
+                src="img/logo/Logo1.png"
+                class="rounded d-block"
+                alt="Logo"
+              />
+
+              <p>
+                Selamat datang di web secreto Custom. Kami menyediakan service
+                terbaik di kota prestige dan berbagai sparepart mobil dan motor
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- about -->
+    <main>
+      <section id="about">
+        <div class="container about">
+          <h1>Tentang kami</h1>
+          <div class="d-flex row justify-content-center">
+            <img
+              data-aos="fade-up"
+              data-aos-delay="500"
+              data-aos-duration="500"
+              src="img/logo/logo4.png"
+              class="mx-5"
+              salt="gambar"
+            />
+            <p class="prag w-50 col" style="text-align: justify">
+              Bengkel ini di buat di kota Prestige Roleplay, bengkel pertama di
+              kota ini, sebelum derjadi pengeboman di bebrapa instansi
+              pemerintah salah satunya bengkel secreto, sebelum berganti nama
+              menjadi secreto bengkel ini adalah bengkel milo, bengkel ini sudah
+              berpindah tempat beberapa kali di karenakan terjadi kebakaran,
+              yang pertama berada di kota, paleto, laut paleto...... 
+              pendiri bengkel dari pertama di buat adalah Milo Slebew
+              dan Wakilnya Vin Solar, dan sekarang sudah beberapa pergantian
+              kepemimpinan, Bos bengkel yang sekarang adalah Bangkit Slebew,
+              Wakilnya Bben dan Vin Solar, sekertaris Helen Voldem.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <!-- gallery -->
+      <section id="gallery">
+        <div class="container gallery">
+          <h1 class="heading">Gallery</h1>
+          <div class="gallery-image">
+            <div
+              class="img-box"
+              data-aos="fade-up-right"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/G1.png" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>Secreto Custom</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="img-box"
+              data-aos="fade-up"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/G2.png" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>Secreto Custom</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="img-box"
+              data-aos="fade-up-left"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/G3.png" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>Secreto Custom</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="img-box"
+              data-aos="fade-up-right"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/gallery/civic type r.jpg" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>Civic Type R</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="img-box"
+              data-aos="fade-up"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/gallery/civic.jpg" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>Civic Sedan</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="img-box"
+              data-aos="fade-up-left"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/gallery/Gallery6.jpg" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>AUDI RS 6 MANSAUG</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="img-box"
+              data-aos="fade-up-right"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/gallery/hondaestilo.jpg" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>CIVIC EK9</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="img-box"
+              data-aos="fade-up"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/gallery/lambo.jpg" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>DIABLO GTR</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="img-box"
+              data-aos="fade-up-left"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/gallery/r34 rusak.jpg" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>Nissan R34</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="img-box"
+              data-aos="fade-up-right"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/gallery/r34.jpg" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>Nissan R34</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="img-box"
+              data-aos="fade-up"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/gallery/supra.jpg" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>Toyota Supra</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="img-box"
+              data-aos="fade-up-left"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/gallery/toyota 86.jpg" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>Toyota 86</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="img-box"
+              data-aos="fade-up-right"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/gallery/Vario.jpg" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>Vario</p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="img-box"
+              data-aos="fade-up-left"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img src="img/gallery/yarisgr.jpg" alt="gambar" />
+              <div class="transparent-box">
+                <div class="caption">
+                  <p>Yaris GR</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <!-- produk -->
+      <section id="produk">
+        <div class="container engine">
+          <h1>Produk</h1>
+          <div class="d-flex flex-wrap gap-4 justify-content-center">
+            <div
+              class="card"
+              style="width: 18rem"
+              class="card-body"
+              class="img-box"
+              data-aos="fade-up"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img
+                src="img/mesin/mesin 2jz.png"
+                class="card-img-top p-2"
+                alt="gambar"
+              />
+              <div class="card-body">
+                <h5 class="card-title">$3.500.000</h5>
+                <h5 class="card-title">2JZ</h5>
+
+                <a href="#" class="btn btn-primary"
+                  ><span class="material-symbols-outlined">
+                    shopping_cart_checkout
+                  </span></a
+                >
+              </div>
+            </div>
+
+            <div
+              class="card"
+              style="width: 18rem"
+              class="card-body"
+              class="img-box"
+              data-aos="fade-up"
+              data-aos-delay="500"
+              data-aos-duration="500"
+            >
+              <img
+                src="img/Mesin/mesin lambo v10.png"
+                class="card-img-top p-2"
+                alt="gambar"
+              />
+              <div class="card-body">
+                <h5 class="card-title">$5.000.000</h5>
+                <h5 class="card-title">Lambo V10</h5>
+
+                <a href="#" class="btn btn-primary"
+                  ><span class="material-symbols-outlined">
+                    shopping_cart_checkout
+                  </span></a
+                >
+              </div>
+            </div>
+
+            <div
+              class="card"
+              style="width: 18rem"
+              class="card-body"
+              class="img-box"
+              data-aos="fade-up"
+              data-aos-delay="500"
+              data-aos-duration="500"
+            >
+              <img
+                src="img/mesin/mesin mus v8.png"
+                class="card-img-top p-2"
+                alt="gambar"
+              />
+              <div class="card-body">
+                <h5 class="card-title">$2.500.000</h5>
+                <h5 class="card-title">Mus V8</h5>
+
+                <a href="#" class="btn btn-primary"
+                  ><span class="material-symbols-outlined">
+                    shopping_cart_checkout
+                  </span></a
+                >
+              </div>
+            </div>
+
+            <div
+              class="card"
+              style="width: 18rem"
+              class="card-body"
+              class="img-box"
+              data-aos="fade-up"
+              data-aos-delay="1000"
+              data-aos-duration="500"
+            >
+              <img
+                src="img/mesin/mesin predator v8.png"
+                class="card-img-top p-2"
+                alt="gambar"
+              />
+              <div class="card-body">
+                <h5 class="card-title">$2.000.000</h5>
+                <h5 class="card-title">Predator V8</h5>
+
+                <a href="#" class="btn btn-primary"
+                  ><span class="material-symbols-outlined">
+                    shopping_cart_checkout
+                  </span></a
+                >
+              </div>
+            </div>
+          </div>
+          <div class="button">
+            <button type="button" class="btn btn-primary btn-lg my-3">
+              <a href="produk.php">Produk lainnya >></a>
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <!-- contact -->
+      <section id="contact">
+        <div class="container contact">
+          <div class="row">
+            <h1>Contact</h1>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Nama"
+                aria-label="nama"
+              />
+            </div>
+            <div class="col">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Email"
+                aria-label="email"
+              />
+            </div>
+            <div class="pesan col-12 mt-3">
+              <textarea
+                class="form-control"
+                placeholder="Pesan"
+                aria-label="Pesan"
+                style="height: 200px; resize: none"
+              ></textarea>
+            </div>
+          <!-- </div>
+          <button class="btn">Button</button>
+        </div> -->
+      </section>
+    </main>
+
+    <!-- FOOTER -->
+    <section id="footer">
+      <footer>
+        <div class="container-footer">
+          <div class="socialicons d-flex">
+            <a href="https://www.instagram.com/secreto.custom/"
+              ><i
+                ><img
+                  width="60"
+                  height="60"
+                  src="img/logo/Instagram.png"
+                  alt="logo" /></i
+            ></a>
+            <a href="https://discord.gg/uk8qY9dk"
+              ><i
+                ><img
+                  width="60"
+                  height="60"
+                  src="img/logo/Discord.png"
+                  alt="logo" /></i
+            ></a>
+            <a href="https://prestigeworld.id/"
+              ><i
+                ><img
+                  width="60"
+                  height="60"
+                  src="img/logo/Prestige.png"
+                  alt="logo" /></i
+            ></a>
+            <a href="https://galangdhaffa.github.io/itw2023_project2_233040024/"
+              ><i
+                ><img
+                  width="60"
+                  height="60"
+                  src="img/logo/logo4.png"
+                  alt="logo" /></i
+            ></a>
+          </div>
+          <div class="footernav">
+            <ul>
+              <li><a href="#home">Home</a></li>
+
+              <li><a href="#about">About</a></li>
+
+              <li><a href="#gallery">Gallery</a></li>
+
+              <li><a href="#produk">produk</a></li>
+
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="footer-bottom">
+          <p>
+            CopyRight &copy;2023; Designed by
+            <span class="Designer">Dhaffa Galang Fahriza</span>
+          </p>
+        </div>
+      </footer>
+    </section>
+  </body>
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"
+  ></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+</html>
